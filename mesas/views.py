4edb6cb3a28cc91c2cc7b833ae3685a13b005218
JesusMedina19 <jesusmedina010405@gmail.com>
2025-01-30 16:41:41 -0500

    Vista para agregar pisos

--- mesas/views.py
+++ mesas/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import *
+from .models import *
+from .forms import *
 def index(request):
-    return render(request,'mesas/index.html')
+    pisos = Piso.objects.all()
+    return render(request, 'mesas/index.html', {'pisos': pisos})
+
+def agregar_piso(resquest):
+    if resquest.method == 'POST':
+        form = PisoForm(resquest.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return redirect('index')
     
