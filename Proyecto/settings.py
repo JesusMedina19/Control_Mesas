8ae6bebe38b105c7ea0891abb159f74632fa55a1
JesusMedina19 <jesusmedina010405@gmail.com>
2025-01-30 12:16:01 -0500

    Region y idiomas actualizados

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'America/Bogotà'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -104,7 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
